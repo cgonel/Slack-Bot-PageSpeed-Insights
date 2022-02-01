b984a18db5a5036201e300793a20f464b6272377
Chris Gonel <gonelw@gmail.com>
2022-01-31 19:27:39 -0500

    send psi report

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
-const { App } = require('@slack/bolt'); 
+import pkg from '@slack/bolt';
+const { App } = pkg;
+import dotenv from 'dotenv';
+import * as service from './service.js';
+
+dotenv.config();
 
 const app = new App({
     token: process.env.SLACK_TOKEN,
@@ -8,7 +13,33 @@ const app = new App({
 app.command('/psi', async ({command, ack, respond}) => {
     await ack();
 
-    await respond(`${command.text}`);
+    const psiReport = await service.psiReport(`${command.text}`)
+
+    await respond({
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": `*Summary* \n*URL*: ${psiReport.url}  \n *Strategy*: mobile\n*Performance*: 10`
+                }
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "*Field Data* \nCumulative Layout Shift (CLS)              | 33ms\nFirst Contentful Paint (FCP)                  | 1.2s\nFirst Input Delay (FID)                           | 22ms\nLargest Contentful Paint (LCP)             | 2.3s"
+                }
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "*Lab Data*\nCumulative Layout Shift                        | 0.322\nFirst Contentful Paint                            | 3.1s\nLargest Contentful Paint                       | 15.7s\nSpeed Index                                           | 27.6s\nTime to Interactive                                | 75.3s\nTotal Blocking Time                               | 9,420ms"
+                }
+            }
+        ]
+    });
 });
 
 (async () => {
--- src/service.ts
+++ src/service.ts
@@ -1,8 +1,56 @@
-const psi = require('psi');
-const { dotenv } = require('dotenv').config();
+// const psi = require('psi');
+// const { dotenv } = require('dotenv').config();
 
-let psiReport = (async (url) => {
-    await psi.output(url, {
+import psi from 'psi';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+class psiReports {
+    private _url: string;
+    private _strategy: string;
+    private _performance: number;
+
+    constructor(url, strategy?, performance?){
+        this._url = url;
+        this._strategy = strategy;
+        this._performance = performance;
+    }
+
+    get url(){
+        return this._url;
+    }
+    get strategy(){
+        return this._strategy;
+    }
+    get performance(){
+        return this._performance;
+    }
+}
+
+
+export const psiReport = async (url) => {
+    const { data } = await psi(url, {
         key: process.env.API_KEY
     });
-})();
+
+    let urlReport = new psiReports(
+        data.lighthouseResult.requestedUrl
+    )
+
+    return urlReport;
+};
+
+try {
+        (async () => {
+            // Get the PageSpeed Insights report
+            const data = await psiReport('https://theverge.com');
+            // console.log('Speed score:', data.lighthouseResult.categories.performance.score);          
+            console.log(data.url)
+            console.log(data.strategy)
+            console.log(data.strategy)
+        })();    
+}catch(err){
+    console.error(err)
+}
+
